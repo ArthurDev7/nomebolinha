e014ba9bf6708bbfad6099d5bf26b8c8877acafc
karlagomes7 <karlagn7@gmail.com>
2020-06-19 16:47:41 -0300

    arthur bolinha

---
+++ js/main.js
@@ -0,0 +1,65 @@
+
+
+var d = new Date();
+alert(d);
+
+
+
+/*
+var count;
+for(count=0; count <5; count++){
+    alert(count)
+};
+*/
+
+/*
+var count = 0;
+while (count < 5){
+    console.log(count);
+    count = count + 1; 
+};
+*/
+/*
+var idade = prompt("Qual sua idade");
+if(idade >= 18){
+   alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+
+
+/*
+var fruta = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(fruta);
+alert(fruta[1].nome);
+*/
+
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" . "));
+
+
+//var nome = "Karla Gomes";
+//var n1 = "3";
+//var n2 = "5";
+//var frase = "japao é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos ");
+//alert (idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log (frase.tolwercase());
+//alert(frase.replace("japao", "Brasil"));
